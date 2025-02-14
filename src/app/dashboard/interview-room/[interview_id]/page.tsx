2e7527bfaf0949e8813d2440ebab1e1b19a12569
adarsh2510 <adarsh25trivedi@gmail.com>
2025-02-14 08:37:51 +0530

    fix: refactor cont.

--- src/app/dashboard/interview-room/[interview_id]/page.tsx
+++ src/app/dashboard/interview-room/[interview_id]/page.tsx
@@ -1,85 +1,70 @@
 'use client'
-
-import QuestionSection from "@/components/interview-room/QuestionSection";
-import SubmitAnswer from "@/components/interview-room/SubmitAnswer";
-import { QuestionType } from "@/constants/questions";
-import { getNextQuestion, submitAnswer } from "@/lib/api/getInterviewData";
 import { IGetNextQuestionResponse } from "@/lib/api/types";
-import { useState, useEffect } from "react";
-import Interviewer from "@/components/interview-room/Interviewer";
+import { useState, useRef } from "react";
+import { QuestionType } from "@/constants/questions";
+import QuestionSection from "@/components/InterviewScene/QuestionSection";
+import InterviewControllers from "@/components/InterviewScene/InterviewControllers";
+import { getNextQuestion, submitAnswer } from "@/lib/api/getInterviewData";
 
 export default function InterviewRoom({params}: {params: {interview_id: string}}) {    
     const { interview_id } = params;
-    const [isInterviewCompleted, setIsInterviewCompleted] = useState(false);
-    const [currentQuestionData, setCurrentQuestionData] = useState<IGetNextQuestionResponse | null>(null);
-    const [currentAnswer, setCurrentAnswer] = useState<string | null>(null);
-    const [isQuestionAsked, setIsQuestionAsked] = useState(false);
-    const [questionType, setQuestionType] = useState<QuestionType>(QuestionType.INITIAL);
-    const [followUpQuestionId, setFollowUpQuestionId] = useState<string | null>(null);
-    const currentQuestion = currentQuestionData?.next_question ?? {
-        question_name: 'Hello, how are you?',
-        question_text: 'Hello, how are you?',
-        question_type: QuestionType.INITIAL,
-        is_last_question: false,
-        is_interview_completed: false
-    };
+    const [currentQuestion, setCurrentQuestion] = useState<IGetNextQuestionResponse | undefined>(undefined);
+    // const questionType = useRef<QuestionType>(QuestionType.INITIAL);
+    const followUpQuestionId = useRef<string | null>(null);
+    console.log('re rendered');
 
-    const handleIsQuestionAsked = (is_question_asked: boolean) => {
-        setIsQuestionAsked(is_question_asked);
+    const handleNextQuestion = async () => {
+        getNextQuestion({ user_id: 'test-user-id', interview_id: interview_id }).then((data)=>{
+            if (data.is_interview_completed) {
+                setCurrentQuestion(undefined);
+            } else {
+                setCurrentQuestion(data);
+            }
+        })
     }
 
-    const handleSubmitAnswer = (answer: string) => {
+     const handleSubmitAnswer = (answer: string) => {
         console.log('recieved answer', answer);
-        setCurrentAnswer(answer);
         submitAnswer({
             user_id: 'test-user-id',
             interview_id: interview_id,
-            question_type: questionType,
+            question_type: currentQuestion?.question_type ?? QuestionType.INITIAL,
             user_response: answer,
-            follow_up_question_id: followUpQuestionId ?? undefined
+            follow_up_question_id: followUpQuestionId.current ?? undefined
         }).then((data)=>{
             console.log('recieved data', data);
             if (data.follow_up_question) {
-                setFollowUpQuestionId(Object.keys(data.follow_up_question)[0] ?? null);
-                setCurrentQuestionData({
+                followUpQuestionId.current = Object.keys(data.follow_up_question)[0] ?? null;
+                setCurrentQuestion({
                     question_type: QuestionType.FOLLOW_UP,
                     next_question: Object.values(data.follow_up_question)[0],
                     is_last_question: false,
                     is_interview_completed: false
                 });
-                setQuestionType(QuestionType.FOLLOW_UP);
             }
             else{
-                setFollowUpQuestionId(null);
+                followUpQuestionId.current = null;
                 getNextQuestion({ user_id: 'test-user-id', interview_id: interview_id }).then((data)=>{
                     if (data.is_interview_completed) {
-                        setIsInterviewCompleted(true);
+                        setCurrentQuestion(undefined);
                     } else {
-                        setCurrentQuestionData(data);
-                        setQuestionType(QuestionType.INITIAL);
+                        setCurrentQuestion(data);
                     }
                 });
             }
         });
     }
 
-    useEffect(() => {
-        getNextQuestion({ user_id: 'test-user-id', interview_id: interview_id }).then((data)=>{
-            if (data.is_interview_completed) {
-                setIsInterviewCompleted(true);
-            } else {
-                setCurrentQuestionData(data);
-                setQuestionType(QuestionType.INITIAL);
-            }
-        });
-    }, []);
-    
-return (
-    <div>
-      <h1>Interview Room for {interview_id}</h1>
-      <QuestionSection currentQuestion={currentQuestion} handleIsQuestionAsked={handleIsQuestionAsked} questionType={questionType}/>
-      <Interviewer/>
-      <SubmitAnswer handleSubmitAnswer={handleSubmitAnswer}/>
-    </div>
-  );
+    const handleUserResponse = async (response: string) => {
+        console.log(response);
+        handleSubmitAnswer(response);
+    }
+
+    return (
+        <div>
+            <h1>Interview Room</h1>
+            <QuestionSection currentQuestion={currentQuestion}/>
+            <InterviewControllers handleNextQuestion={handleNextQuestion} handleUserResponse={handleUserResponse}/>
+        </div>
+    )
 }
--- src/components/InterviewScene/InterviewControllers.tsx
+++ src/components/InterviewScene/InterviewControllers.tsx
@@ -1,8 +1,12 @@
-const InterviewControllers = ({handleNextQuestion, handleSubmitAnswer}: {handleNextQuestion: () => void, handleSubmitAnswer: (response: string) => void}) => {
+import { useState } from "react";
+
+const InterviewControllers = ({handleNextQuestion, handleUserResponse}: {handleNextQuestion: () => void, handleUserResponse: (response: string) => void}) => {
+    const [userResponse, setUserResponse] = useState('');
     return (
         <div>
-            <button style={{backgroundColor: 'green', color: 'white'}} onClick={handleNextQuestion}>Next Question</button>
-            <button style={{backgroundColor: 'red', color: 'white'}} onClick={() => handleSubmitAnswer('test response')}>Submit Answer</button>
+            <button style={{backgroundColor: 'green', color: 'white'}} onClick={handleNextQuestion}>Start Interview</button>
+            <input type="text" onChange={(e) => setUserResponse(e.target.value)} />
+            <button style={{backgroundColor: 'red', color: 'white'}} onClick={() => handleUserResponse(userResponse)}>Submit Answer</button>
         </div>
     )
 }
